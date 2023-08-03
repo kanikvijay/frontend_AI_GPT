418b6c08190b9c515642d562b1c2766a49cc88b6
kanikvijay <76864232+kanikvijay@users.noreply.github.com>
2023-08-03 11:03:54 +0530

    Update Register.js

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -28,7 +28,7 @@ const Register = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      await axios.post("/api/v1/auth/register", { username, email, password });
+      await axios.post("https://backend-ai-gpt.onrender.com/api/v1/register", { username, email, password });
       toast.success("User Register Successfully");
       navigate("/login");
     } catch (err) {
